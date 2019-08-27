3a3f41056cf76e4956c0320d412ed2bff0e940fd
sevenjinxu <sevenjinxu>
2019-08-27 18:21:43 +0800

    更新自定义导航栏

---
+++ components/icon/index.js
@@ -0,0 +1,48 @@
+Component({
+  options: {
+    addGlobalClass: true
+  },
+
+  externalClasses: ['custom-class'],
+
+  properties: {
+    info: null,
+    name: String,
+    size: String,
+    color: String,
+    block: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  methods: {
+    onClick() {
+      this.triggerEvent('click');
+    }
+  }
+});
+Component({
+  options: {
+    addGlobalClass: true
+  },
+
+  externalClasses: ['custom-class'],
+
+  properties: {
+    info: null,
+    name: String,
+    size: String,
+    color: String,
+    block: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  methods: {
+    onClick() {
+      this.triggerEvent('click');
+    }
+  }
+});
